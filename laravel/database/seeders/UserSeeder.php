2fb0f36b2cf7b1d9e14f69f323b17584e804a62b
FatihIndraa <fatihindra140903@gmail.com>
2024-04-29 22:32:34 +0700

    update data

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-
 class UserSeeder extends Seeder
 {
     /**
@@ -13,25 +12,29 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-       $userData = [
-        [
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'role' => 'admin',
-            'password' => bcrypt('password'),
-        ],
-        [
-            'name' => 'pasien',
-            'email' => 'pasien@pasien.com',
-            'role' => 'pasien',
-            'password' => bcrypt('password'),
-        ],
-        [
-            'name' => 'dokter',
-            'email' => 'dokter@dokter.com',
-            'role' => 'dokter',
-            'password' => bcrypt('password')
-        ],
-       ];
+        $userData = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'role' => 'admin',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'pasien',
+                'email' => 'pasien@pasien.com',
+                'role' => 'pasien',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'dokter',
+                'email' => 'dokter@dokter.com',
+                'role' => 'dokter',
+                'password' => bcrypt('password')
+            ],
+           ];
+
+           foreach ($userData as $key => $value) {
+               User::create($value);
+           }
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,24 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SesiController;
-use App\Http\Controllers\AdminController;
 
-Route::middleware(['guest'])->group(function () {
-    Route::get('/', [SesiController::class, 'index'])->name('login');
-    Route::post('/', [SesiController::class,'login']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::get('/home', function(){
-    return redirect('/admin');
-});
-
-Route::middleware(['auth'])->group(function(){
-    Route::get('/admin', [AdminController::class, 'index']);
-    Route::get('/admin/admin', [AdminController::class, 'admin'])->middleware('userAkses:admin');
-    Route::get('/admin/dokter', [AdminController::class, 'dokter'])->middleware('userAkses:dokter');
-    Route::get('/admin/pasien', [AdminController::class, 'pasien'])->middleware('userAkses:pasien');
-    Route::get('/logout', [SesiController::class, 'logout']);
-});
-
-
