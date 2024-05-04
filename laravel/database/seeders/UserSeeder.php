7f9c5e6ec638108cf8bdd57f15134c505973c2e3
N3xxus <129999096+FatihIndraa@users.noreply.github.com>
2024-05-04 18:07:48 +0700

    update seeder

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Operator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
@@ -16,26 +17,37 @@ class UserSeeder extends Seeder
     {
         $userData = [
             [
-                'name' => 'Admin',
-                'email' => 'admin@admin.com',
-                'password' => bcrypt('password'),
-            ],
-            [
                 'name' => 'Pasien',
                 'email' => 'pasien@pasien.com',
-                'gender' => 'Laki-laki',
-                'alamat' => 'Jalan Pasien No. 1',
+                'gender'=>'Laki-laki',
                 'password' => bcrypt('password'),
-            ],
-            [
-                'name' => 'Dokter',
-                'email' => 'dokter@dokter.com',
-                'password' => bcrypt('password')
-            ],
+            ]
         ];
         
         foreach ($userData as $value) {
             User::create($value);
+        }
+        $dokterData = [
+            [
+                'name' => 'dokter',
+                'email' => 'dokter@dokter.com',
+                'password' => bcrypt('password'),
+            ]
+        ];
+        
+        foreach ($dokterData as $value) {
+            dokter::create($value);
+        }
+        $operatorData = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password'),
+            ]
+        ];
+        
+        foreach ($operatorData as $value) {
+            Operator::create($value);
         }
     }
 }
